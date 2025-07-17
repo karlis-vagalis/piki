a66297b334ea659cb93061e52ad9ec638a7c34c0
VAGALIS, Karlis <karlis.vagalis@gmail.com>
2025-07-17 20:47:48 +0200

    Updated seed UUIDs

--- db/migrations/0_init.sql
+++ db/migrations/0_init.sql
@@ -1,12 +1,13 @@
 -- Add admin account if it does exist yet
 INSERT INTO "accounts" ("id", "name", "type")
-VALUES ('00000000-0000-7000-8000-000000000000', 'admin', 'service')
+VALUES ('00000000-0000-7000-8000-830000000000', 'admin', 'service')
 ON CONFLICT ("id") DO NOTHING;
 
+-- Create top level all_accounts group
 INSERT INTO "groups" ("managed_by", "id", "name", "description", "parent_id", "left", "right")
 VALUES (
-    '00000000-0000-7000-8000-000000000000',
-    '00000000-0000-7000-8000-000000000001',
+    '00000000-0000-7000-8000-830000000000',
+    '00000000-0000-7000-8000-830000000001',
     'All accounts',
     'Top-level group containing all accounts',
     NULL,
